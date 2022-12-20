14160edb3aa01507a633ca020462db78b7da2584
yavuzyarkinokular <yarkinokular@gmail.com>
2022-12-20 16:54:12 +0300

    deleted old files

--- degisken.js
+++
@@ -1,9 +0,0 @@
-var yasim = 31;
-
-console.log(yasim);
-
-let telefon = 5068237354;
-console.log(telefon);
-
-const idnumber = 41836529716;
-console.log(idnumber);
--- operatorler.js
+++
@@ -1,4 +0,0 @@
-//Aritmetik operatörler: + - * / % ++ --
-// Nan: Not a Number anlamına gelmektedir. String ifadelerde matematiksel işlemler yapılamadığı için Nan değerini alır.
-// % Mod operatörüdür. Bölümünden kalanı verir.
-// ++ ve -- operatörleri birim arttırır veya birim azaltır.
